d3e38c46923d687037e1ef0cd1c5f04db6e0a846
Alex990407 <zhelezniako07@gmail.com>
2024-08-12 12:54:41 +0200

    hw 3 React. First part.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Rating from "./components/Rating";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Rating</h1>
+      <Rating />
+    </div>
+  );
+}
+
+export default App;
